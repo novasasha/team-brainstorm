07c5c04ae30755885c9496488ac7010178d9f376
Matthew Moran <email": null>
2016-08-05 17:04:03 -0500

    add functioning get_card method

--- app/models/card.rb
+++ app/models/card.rb
@@ -2,4 +2,12 @@ class Card < ActiveRecord::Base
   # Remember to create a migration!'
   belongs_to :deck
   has_many :guesses
+
+  def get_guesses
+    guesses = self.guesses.to_a
+    guesses.map! do |guess|
+      guess.correct
+    end
+    guesses
+  end
 end
